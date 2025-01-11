3fa004b227126e4c84f251682e7af89cd93146c3
Mustafa Emir ATA <148717139+mustafaemirata@users.noreply.github.com>
2025-01-11 15:34:53 +0300

    Binary Search Tree
    Insert, Delete, findMax, findMin, Print fonksionarıyla temel ağaç yapısı oluşturuldu.

---
+++ binarySearchTree.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+Node *createNode(int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+Node *insert(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        return createNode(data);
+    }
+    if (data < root->data)
+    {
+        root->left = insert(root->left, data);
+    }
+    else if (data > root->data)
+    {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+void inorderTraversal(Node *root)
+{
+    if (root != NULL)
+    {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+
+Node *search(Node *root, int key)
+{
+    if (root == NULL || root->data == key)
+    {
+        return root;
+    }
+    if (key < root->data)
+    {
+        return search(root->left, key);
+    }
+    return search(root->right, key);
+}
+
+Node *findMin(Node *root)
+{
+    while (root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root;
+}
+
+Node *findMax(Node *root)
+{
+    while (root->right != NULL)
+    {
+        root = root->right;
+    }
+    return root;
+}
+
+Node *deleteNode(Node *root, int key)
+{
+    if (root == NULL)
+    {
+        return root;
+    }
+    if (key < root->data)
+    {
+        root->left = deleteNode(root->left, key);
+    }
+    else if (key > root->data)
+    {
+        root->right = deleteNode(root->right, key);
+    }
+    else
+    {
+        if (root->left == NULL)
+        {
+            Node *temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL)
+        {
+            Node *temp = root->left;
+            free(root);
+            return temp;
+        }
+        Node *temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+int main()
+{
+    Node *root = NULL;
+
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    printf("In-order Traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    int key = 40;
+    Node *result = search(root, key);
+    if (result != NULL)
+    {
+        printf("%d bulundu.\n", key);
+    }
+    else
+    {
+        printf("%d bulunamadı.\n", key);
+    }
+
+    printf("Silme işlemi: 50\n");
+    root = deleteNode(root, 50);
+
+    printf("In-order Traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
